92e2982bca7af3be762061b0483cd83538861bc5
Eric C Schoenborn <cultureondemand@gmail.com>
2024-04-23 14:03:54 -0400

    Add files via upload

---
+++ myplan_content.js
@@ -0,0 +1,57 @@
+var slideData = [
+
+  
+ 
+  {
+    section: "Kinetic Cartoon",
+//////////////////     icon: "pcy.svg",
+header: "",
+    video: "yes",
+   bkgImage: {
+     wm: "assets/images/bkg/project.webm",
+       mp: "assets/images/bkg/project.mp4",
+      name:"1"
+    },
+    body: `
+<h1>Commenting on automation and innovation culture, this kinetic sculpture interacts with a digital animation that illustrates the impact of automation on huanity ... in this case driving up unemployment.</h1>  
+    `
+  },
+  
+
+  {
+    section: "Online Story Offline Mechanics",
+//////////////////     icon: "pcy.svg",
+    header: "",
+ //////////////////////////   video: "yes",
+    bkgImage: {
+      small: "piece.png",
+      medium: "piece.png",
+      large: "piece.png"
+      
+    },
+    body: `
+<strong>Laser Cut Characters and Cam System, WebGL Interactive Storyworld, Custom AR Markers, Webcam</strong><br />     
+    `
+  },
+  
+
+  
+   
+  {
+    section: "Destroy Jobs with a Crank",
+//////////////////     icon: "pcy.svg",
+header: "",
+    video: "yes",
+   bkgImage: {
+     wm: "assets/images/bkg/piece.webm",
+       mp: "assets/images/bkg/piece.mp4",
+      name:"2"
+      
+    },
+    body: `
+<strong>Apple Basic and early graphics programming</strong><br /> 
+    `
+  } 
+
+
+];
